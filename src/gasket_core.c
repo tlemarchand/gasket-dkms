2a50e2b2d78afc90ca8209fb9539f0de39837633
Jeff Mahoney <jeffm@jeffm.io>
2021-09-06 21:49:32 -0400

    gasket: restore ability to unload module
    Commit 89ff180f96d (staging: gasket: remove gasket_exit()) did as
    advertised.  While removing empty functions is generally a good
    cleanup, the absence of a module_exit call means the module can't
    be unloaded.  Since gasket doesn't have any resources to release,
    it's easily unloaded once the empty exit call is restored.
    
    Signed-off-by: Jeff Mahoney <jeffm@suse.com>

--- src/gasket_core.c
+++ src/gasket_core.c
@@ -1921,8 +1921,17 @@ static int __init gasket_init(void)
 	return 0;
 }
 
+static int __exit gasket_exit(void)
+{
+	/*
+	 * No resources of our own to release, but we can't be unloaded
+	 * without it.
+	 */
+}
+
 MODULE_DESCRIPTION("Google Gasket driver framework");
 MODULE_VERSION(GASKET_FRAMEWORK_VERSION);
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Rob Springer <rspringer@google.com>");
 module_init(gasket_init);
+module_exit(gasket_exit);
